b11121d2d53d967fc6487528e94fb8107c6f4493
RyanMcC28 <ryancallion@aol.com>
2024-09-12 22:07:41 +0100

    Update

---
+++ organiser.py
@@ -0,0 +1,17 @@
+import os 
+
+cwd = os.getcwd()
+
+
+file_list = list(os.listdir(cwd))
+
+sorted_files = sorted(file_list, reverse=True)
+
+for item in sorted_files:
+    print(item)
+    index = sorted_files.index(item)
+    
+    
+    
+#os.rename(fr"{cwd}\Variables Guide", "Variables Guide.py")
+#os.rename(fr"{cwd}\Slot Machine MK2.py", "Slot_Machine.py")
