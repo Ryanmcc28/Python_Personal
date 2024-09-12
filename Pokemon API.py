a769cd0a75d925af260fb03bed0d317413098724
RyanMcC28 <ryancallion@aol.com>
2024-09-12 20:46:22 +0100

    TEST

---
+++ Pokemon API.py
@@ -0,0 +1,22 @@
+import requests
+
+api_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon(name):
+    url = f"{api_url}/pokemon/{name}"
+    response = requests.get(url)
+    
+    if  response.status_code == 200:
+        print("Data retrieved")
+        pokedata = response.json()
+        return pokedata
+    else:
+        print("Failed to print data")
+
+
+pokemon_name = "428"
+pokeinfo = get_pokemon(pokemon_name)
+
+if  pokeinfo:
+    print(f"{pokeinfo["name"]}")
+
